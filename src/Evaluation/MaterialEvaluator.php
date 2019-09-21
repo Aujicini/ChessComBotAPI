a9b701522386d119c17a0c5b282cc01171fa29de
oxuwazet <55375045+oxuwazet@users.noreply.github.com>
2019-09-21 14:53:36 -0400

    Add material evaluator.

---
+++ src/Evaluation/MaterialEvaluator.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Aura\Evaluator;
+
+use Aura\Chessboard;
+use Aura\Evaluator;
+
+final class MaterialEvaluator implements Evaluator
+{
+    public function evaluate(Chessboard $board): int
+    {
+        // array_reduce is cleaner, but performance is the key here
+        $sum = 0;
+        foreach ($board->board() as $piece) {
+            $sum += $this->getPieceValue($piece);
+        }
+
+        return $sum;
+    }
+
+    private function getPieceValue(?array $piece): int
+    {
+        if ($piece === null) {
+            return 0;
+        }
+        $color = $piece['color'] === 'w' ? 1 : -1;
+
+        switch ($piece['type']) {
+            case 'p':
+                return PointValue::PAWN * $color;
+            case 'r':
+                return PointValue::ROOK * $color;
+            case 'n':
+                return PointValue::KNIGHT * $color;
+            case 'b':
+                return PointValue::BISHOP * $color;
+            case 'q':
+                return PointValue::QUEEN * $color;
+            case 'k':
+                return PointValue::KING * $color;
+            default:
+                return 0;
+        }
+    }
+
+}
