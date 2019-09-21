63759fcf89aee38f6e9515b0dac3975ba58a915c
oxuwazet <55375045+oxuwazet@users.noreply.github.com>
2019-09-21 14:58:14 -0400

    Add material point value.

---
+++ src/Material/PointValue.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Aura\Material;
+
+interface PointValue
+{
+    public const PAWN = 75;
+    public const KNIGHT = 300;
+    public const BISHOP = 400;
+    public const ROOK = 500;
+    public const QUEEN = 1035;
+    public const KING = 10000;
+}
