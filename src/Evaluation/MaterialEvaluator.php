3a2f946e0170e37302e5016ff2f7163fdce880dc
oxuwazet <55375045+oxuwazet@users.noreply.github.com>
2019-09-26 16:33:10 -0400

    Update MaterialEvaluator.php

--- src/Evaluation/MaterialEvaluator.php
+++ src/Evaluation/MaterialEvaluator.php
@@ -1,46 +1,81 @@
 <?php declare(strict_types=1);
+/**
+ * Aura - A powerful 4 player chess engine.
+ *
+ * @link <https://github.com/oxuwazet/aura> Source Code.
+ * @link <https://www.chess.com/4-player-chess> 4 Player Chess Online.
+ *
+ * @author Oxuwazet <https://github.com/oxuwazet> Profile Page.
+ *
+ * @license Boost Software License 1.0 <https://www.boost.org/LICENSE_1_0.txt> The license main page.
+ */
 
 namespace Aura\Evaluator;
 
-use Aura\Chessboard;
-use Aura\Evaluator;
+use Phoenix\Zero\FourPlayerChess\Utils;
 
+/**
+ * The material evaluator.
+ */
 final class MaterialEvaluator implements Evaluator
 {
-    public function evaluate(Chessboard $board): int
-    {
-        // array_reduce is cleaner, but performance is the key here
-        $sum = 0;
-        foreach ($board->board() as $piece) {
-            $sum += $this->getPieceValue($piece);
-        }
+    /**
+     * @var array[] $pieceValues A list of values assigned to pieces.
+     */
+    private $pieceValues = [
+        'RP' => 100,
+        'RN' => 300,
+        'RB' => 400,
+        'RR' => 525,
+        'RQ' => 1000,
+        'RK' => 100000,
+        'BP' => -100,
+        'BN' => -300,
+        'BB' => -400,
+        'BR' => -525,
+        'BQ' => -1000,
+        'BK' => -100000,
+        'YP' => 100,
+        'YN' => 300,
+        'YB' => 400,
+        'YR' => 525,
+        'YQ' => 1000,
+        'YK' => 100000,
+        'GP' => -100,
+        'GN' => -300,
+        'GB' => -400,
+        'GR' => -525,
+        'GQ' => -1000,
+        'GK' => -100000,
+    ];
 
+    /**
+     * Evaluate the material on a board.
+     *
+     * @return int Returns the evaluator sum.
+     */
+    public function evaluate(): int
+    {
+        $sum = 0;
+        foreach (Utils::$pieces as $key => $value) {
+            $color = substr($key, 0, 1);
+            $piece = substr($key, 0, 1);
+            if ($piece == 'P') {
+                $piece = $value[1];
+            }
+            $sum += $this->getPieceValue($color . $piece);
+        }
         return $sum;
     }
 
-    private function getPieceValue(?array $piece): int
+    /**
+     * Get the value of the specified piece.
+     *
+     * @return int Returns the piece value.
+     */
+    private function getPieceValue(?string $piece): int
     {
-        if ($piece === null) {
-            return 0;
-        }
-        $color = $piece['color'] === 'w' ? 1 : -1;
-
-        switch ($piece['type']) {
-            case 'p':
-                return PointValue::PAWN * $color;
-            case 'r':
-                return PointValue::ROOK * $color;
-            case 'n':
-                return PointValue::KNIGHT * $color;
-            case 'b':
-                return PointValue::BISHOP * $color;
-            case 'q':
-                return PointValue::QUEEN * $color;
-            case 'k':
-                return PointValue::KING * $color;
-            default:
-                return 0;
-        }
+        return $this->pieceValues[$piece];
     }
 
 }
