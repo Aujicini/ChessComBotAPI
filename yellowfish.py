b95b29b961ef881ddbc1464b2c95ed99a10d2986
Nicholas English <oxuwazet@gmail.com>
2021-02-07 18:23:44 -0500

    Add comments.

--- yellowfish.py
+++ yellowfish.py
@@ -25,7 +25,9 @@ class Search:
 
     def search(self,pos,alpha,beta,depth=3,ply=1):
         self.nodes+=1
+        # Detect mate.
         if pos.dead(): return mate-ply
+        # Preform Mate-Distance Pruning.
         mating_value=mate-ply;
         if mating_value<beta:
             beta=mating_value
@@ -34,48 +36,82 @@ class Search:
         if mated_value>alpha:
             alpha=mated_value
             if beta<=mated_value: return mated_value
+        # Prevent negitave depths in the transposition table.
         depth=max(depth,0)
+        # Check to see if we have visited this position before.
         entry=self.tt_score.get(pos.hash())
         if entry and entry[1]>=depth:
             if entry[0]==0: return entry[2]
             elif entry[0]==-1: alpha=max(alpha,entry[2])
             elif entry[1]==1: beta=min(beta,entry[2])
             if alpha>=beta: return entry[2]
+        # We have reached depth 0. Preform a QSearch.
         if depth==0: return self.qsearch(pos,alpha,beta,ply+1)
+        # We are saving the original alpha score for transposition construction.
         o_alpha=alpha
         best=-infinity
+        # Sort through the moves.
         for move in self.sorted(pos):
+            # We are increamenting the ply to determine how far we went into the search.
+            # This is will be used to prune the mate distance allowing the engine to go for the fastest 
+            # way to mate and the longest way to be mated.
+            # How? We simply return the mate score - ply.
             score=-self.search(pos.move(move),-beta,-alpha,depth-1,ply+1)
             if score>=beta:
+                # This is the best move.
+                # Save this move in the killers table for future searches.
                 self.tt_move[pos.hash()]=move
                 best=score
                 break
             if score>best:
+                # This is the best move.
+                # Save this move in the killers table for future searches.
                 self.tt_move[pos.hash()]=move
                 best=score
                 alpha=max(alpha,score)
+        # If the best score is less than or equal to the original alpha then the position is UPPERBOUND.
+        # If the best score is greater than or equal to the beta then the position is LOWERBOUND.
+        # Else this position is EXACT.
         if best<=o_alpha: self.tt_score[pos.hash()]=(1,depth,best)
         elif best>=beta: self.tt_score[pos.hash()]=(-1,depth,best)
         else: self.tt_score[pos.hash()]=(0,depth,best)
+        # Now we return the best score.
         return best
         
     def qsearch(self,pos,alpha,beta,ply):
         self.nodes+=1
+        # Detect mate.
         if pos.dead(): return mate-ply
+        # Get the current eval score.
         score=pos.score
+        # Preform AB Pruning.
         if score>=beta: return beta
+        # Preform Delta Pruning.
         if score<alpha-delta_margin: return alpha
         alpha=max(alpha,score)
+        # Sort through the moves.
         for move in self.sorted(pos):
+            # Only allow capture moves.
             if pos.board[move[1]]==0: continue
+            # The search is the same, but only for capture moves.
             score=-self.qsearch(pos.move(move),-beta,-alpha,ply+1)
+            # Preform AB Pruning.
             if score>=beta: return beta
             if score>alpha: alpha=score
+        # Return the best score.
         return alpha
 
     def sorted(self,pos):
+        # First we check the killer table to see if have searched this position before
+        # and have determined a best move. When we search we save the best move in the killer table
+        # so the next time the engines searches for the best move we search the best moves from the
+        # previous search to allow for more AB pruning to take place.
         killer=self.tt_move.get(pos.hash())
+        # If a killer was found yield that move first.
         if killer: yield killer
-        for move in sorted(pos.gen_moves(),key=pos.value,reverse=True): 
+        # Now sort the moves by value of the move/
+        # So for example we should be looking at captures/pawn promotions.
+        for move in sorted(pos.gen_moves(),key=pos.value,reverse=True):
+            # If this move matches the killer then continue, we have already yielded the killer move.
             if move==killer: continue
             yield move
