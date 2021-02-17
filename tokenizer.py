79218c2f7ae5fbc9104ce1e1b5dc96eda501ec02
Nicholas English <oxuwazet@gmail.com>
2021-02-17 18:34:23 -0500

    Add comments

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pypy
 # -*- coding: utf-8 -*-
 
-ACCESS_URL="https://4player-beta.chess.com/"
-TOKEN="xxxxxxxxxxxxxxx"
+ACCESS_URL="https://4player-beta.chess.com/" # <- API ENDPOINT
+TOKEN="xxxxxxxxxxxxxxx"                      # <- API TOKEN
